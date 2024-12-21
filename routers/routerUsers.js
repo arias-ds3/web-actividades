b55274aac10413b7176affc805cc1d969cbb8f44
arias-ds3 <ariasfa7@gmail.com>
2024-12-21 15:50:28 -0300

    check user money

---
+++ routers/routerUsers.js
@@ -0,0 +1,21 @@
+let express = require("express")
+
+let routerUsers = express.Router();
+let user = require("../data/users");
+
+routerUsers.post("/check", (req, res) => {
+    let userName = req.body.userName 
+    let password = req.body.password
+
+let user = users.find( u => u.name == userName && u.password == password)
+if ( user == null){
+    res.send("Invalid user")
+    return
+}
+res.send(" "+user.name+" has "+user.money)
+
+})
+
+
+
+module.exports = routerUsers
