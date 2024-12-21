3ef320ef87df7d22ef9e679d305e63b0233d729f
arias-ds3 <ariasfa7@gmail.com>
2024-12-20 21:46:04 -0300

    ficheros de la data

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const app = require("express")
+const express = require("express")
+const app = expres()
+const port = 8081
+
+
+app.listen(port, () => {
+    console.log("Servidor activo en "+port)
+})
